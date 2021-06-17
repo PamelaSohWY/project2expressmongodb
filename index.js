82fe281efb0ade2499f69afcf487ab341d4f3d9c
PamelaSohWY <pamelasohwy@gmail.com>
2021-06-17 05:57:55 +0000

    get endpoint to show search by criteria done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
 const MongoUtil = require('./MongoUtil');
+const { Db } = require('mongodb');
 const mongoUrl = process.env.MONGO_URL;
 
 let app = express();
@@ -36,6 +37,10 @@ async function main() {
         let location_contributor = req.body.location_contributor
         let date_time = new Date(req.body.date_time) || new Date();
         let img_url = req.body.img_url;
+        let service_rating = req.body.service_rating;
+        let food_rating = req.body.food_rating; 
+        let cleanliness_rating=req.body.cleanliness_rating;
+        let average_price = req.body.average_price
         try {
             let Db = MongoUtil.getDB() // add this to activate function //take note Db is in Capital 
             // this section tell mongo to insert the document 
@@ -53,7 +58,10 @@ async function main() {
                 other_details: other_details,
                 location_contributor: location_contributor,
                 date_time: date_time,
-                img_url: img_url
+                img_url: img_url,
+                service_rating : service_rating,
+                cleanliness_rating: cleanliness_rating,
+                avergae_price:average_price
             }); // end of result
             res.status(200); //200 means ok 
             res.send(result);
@@ -71,23 +79,48 @@ async function main() {
 // working when connecting to test
 
 //GET Endpoint to search for hawker locations 
-app.get("/location", async (re, res) => {
+app.get("/locations", async (req, res) => {
     //this lets a variable criteria to be an empty array 
     let criteria = {};
     //This contains the different criterias of search 
     //for each criteria write a new req.query 
     //reference to https://docs.mongodb.com/manual/reference/operator/query/regex/ to determine the different options possible 
     //Possible use for later :db.products.find( { description: { $regex: /m.*line/, $options: 'si' } } )    . Dot Character to Match New Line
+    
+    //Search based on Stall Name 
     if (req.query.stall_name) {
-        criteria = {
-            criteria['stall_name'] = {$regex: req.query.description, $options:"i"}
+        criteria['stall_name'] = { //search based on stall_name
+                $regex: req.query.stall_name, 
+                $options:"i"
         } //end of criteria 
     } //end of stall name query 
 
-    if (req.query.street)
+    // Search based on Street  // note if neccessary change this to general field of address later //TODO!
+    if (req.query.street) {
+        criteria['street'] = { //search based on stall_name
+                $regex: req.query.street, 
+                $options:"i"  //change options later TODO!
+            }//end of criteia regex options
+    } //end of street name query 
 
-    if (req.query.postal_code)
+    if (req.query.food_quality) { 
+        criteria['food_quality'] ={
+            $regex: req.query.food_quality,
+            $options:"i" //change options later TODO!
+        }
+    } //end of food_quality // for this check later if the 3 quality conditons can be aggregated. 
 
+
+    //How do you search by recent date criteria ? TODO! 
+
+    //Show results 
+    let Db = MongoUtil.getDB() //need this to activate 
+    let results = await Db.collection("locations")
+    .find(criteria)
+    .toArray();
+
+    res.status(200);
+    res.send(results);
 
 })
 
@@ -99,3 +132,4 @@ app.listen(3000, () => {
 })
 
 //Reference source of code NodeJS and Express Lab Code - Lab 10 
+//Reference source of code : https://expressjs.com/en/guide/routing.html
